82f60d19939d36815e37965902175ecbf20ec3a1
Thais Araújo <143031924+tthaixs@users.noreply.github.com>
2024-01-20 16:25:05 -0300

    Update ClassesDoJogo.java

--- src/ClassesDoJogo.java
+++ src/ClassesDoJogo.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class ClassesDoJogo {
+    private String nomeHeroi;
+    private String idadeHeroi;
+    private String tipoHeroi;
+    private String ataque;
+
+    public ClassesDoJogo(String tipoHeroi, String ataque){
+        this.tipoHeroi = tipoHeroi;
+        this.ataque = ataque;
+
     }
-}
+
+    public void atacar(){
+        System.out.println("O " + tipoHeroi + " atacou usando " + ataque);
+    }
+
+    public static void main(String[] args) {
+        ClassesDoJogo primeiroTipo = new ClassesDoJogo("mago", "magia");
+        ClassesDoJogo segundoTipo = new ClassesDoJogo("guerreiro", "espada");
+        ClassesDoJogo terceiroTipo = new ClassesDoJogo("monge", "artes marciais");
+        ClassesDoJogo quartoTipo = new ClassesDoJogo("ninja", "shuriken");
+        
+        primeiroTipo.atacar();
+        segundoTipo.atacar();
+        terceiroTipo.atacar();
+        quartoTipo.atacar();
+        
+    }
+} 
